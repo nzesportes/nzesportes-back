ace782e50b6794f88fa816f521d56e5b367480d3
Felipe Loureiro <felipe-loureiro@outlook.com>
2021-03-23 23:05:07 -0300

    first commit

---
+++ src/test/java/br/com/nzesportes/api/nzapi/NzapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nzesportes.api.nzapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NzapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
