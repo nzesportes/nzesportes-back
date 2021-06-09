e38af6bf2e399d8c5d7be8b9f9d23b6b2fc95260
Felipe Loureiro <felipe-loureiro@outlook.com>
2021-06-08 21:01:28 -0300

    AJuste de filtros

--- src/main/java/br/com/nzesportes/api/nzapi/services/product/CategoryService.java
+++ src/main/java/br/com/nzesportes/api/nzapi/services/product/CategoryService.java
@@ -1,16 +1,18 @@
 package br.com.nzesportes.api.nzapi.services.product;
 
+import br.com.nzesportes.api.nzapi.domains.Role;
 import br.com.nzesportes.api.nzapi.domains.product.Category;
 import br.com.nzesportes.api.nzapi.dtos.StatusTO;
 import br.com.nzesportes.api.nzapi.errors.ResourceConflictException;
 import br.com.nzesportes.api.nzapi.errors.ResourceNotFoundException;
 import br.com.nzesportes.api.nzapi.errors.ResponseErrorEnum;
 import br.com.nzesportes.api.nzapi.repositories.product.CategoryRepository;
-import br.com.nzesportes.api.nzapi.security.services.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -26,8 +28,16 @@ public class CategoryService {
         return repository.save(category);
     }
 
-    public Page<Category> getAll(int page, int size, Boolean status, String type, String name) {
-        return repository.findAllByStatusAndTypeContainingAndNameContaining(status, type, name, PageRequest.of(page, size));
+    public Page<Category> getAll(int page, int size, Boolean status, String type, String name, UserDetails principal) {
+        if(type == null)
+            type = "";
+        if(name == null)
+            name = "";
+        if(principal.getAuthorities().contains(new SimpleGrantedAuthority(Role.ROLE_USER.getText())))
+            return repository.findByFilterAndStatus(true, type, name, PageRequest.of(page, size));
+        if(status != null && principal.getAuthorities().contains(new SimpleGrantedAuthority(Role.ROLE_ADMIN.getText())))
+            return repository.findByFilterAndStatus(status, type, name, PageRequest.of(page, size));
+        return repository.findByFilter(type, name, PageRequest.of(page, size));
     }
 
     public HttpStatus deleteById(UUID id) {
