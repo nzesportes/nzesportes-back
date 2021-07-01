8e4858a87ebf8abe396ed96426d2ed7a2732b938
Felipe Loureiro <felipe-loureiro@outlook.com>
2021-06-30 23:19:56 -0300

    ajustando smtp

--- src/main/java/br/com/nzesportes/api/nzapi/dtos/ProductDetailSaveTO.java
+++ src/main/java/br/com/nzesportes/api/nzapi/dtos/ProductDetailSaveTO.java
@@ -1,10 +1,12 @@
 package br.com.nzesportes.api.nzapi.dtos;
 
 import br.com.nzesportes.api.nzapi.domains.product.Gender;
+import br.com.nzesportes.api.nzapi.domains.product.Stock;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -17,4 +19,5 @@ public class ProductDetailSaveTO {
     private Boolean status;
     private UUID productId;
     private Integer quantity;
+    private List<Stock> stock;
 }
