8701376d2dd42f797e249bc49668c115a6e0d33e
Felipe Loureiro <felipe-loureiro@outlook.com>
2021-08-17 22:36:59 -0300

    ajustando o controler de sub categoria

--- src/main/java/br/com/nzesportes/api/nzapi/controllers/product/SubCategoryController.java
+++ src/main/java/br/com/nzesportes/api/nzapi/controllers/product/SubCategoryController.java
@@ -2,9 +2,11 @@ package br.com.nzesportes.api.nzapi.controllers.product;
 
 import br.com.nzesportes.api.nzapi.domains.product.Gender;
 import br.com.nzesportes.api.nzapi.domains.product.SubCategory;
+import br.com.nzesportes.api.nzapi.dtos.SubCategorySaveTO;
 import br.com.nzesportes.api.nzapi.services.product.SubCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
@@ -22,7 +24,7 @@ public class SubCategoryController {
     @PostMapping
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
     public ResponseEntity<SubCategory> create(@RequestBody SubCategorySaveTO subCategory) {
-        return ResponseEntity.ok(service.save(subCategory));
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(subCategory));
     }
 
     @GetMapping("/{id}")
@@ -39,4 +41,15 @@ public class SubCategoryController {
         return service.getAll(name, gender, status, page, size, auth);
     }
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
+    @PutMapping
+    public ResponseEntity<SubCategory> update(@RequestBody SubCategorySaveTO subCategory) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.update(subCategory));
+    }
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable UUID id){
+        service.deleteById(id);
+    }
 }
