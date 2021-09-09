9c9c7e0745fce4ede267ef95956818affddc591e
Felipe Loureiro <felipe-loureiro@outlook.com>
2021-09-08 22:11:18 -0300

    adicionando webhook

--- src/main/java/br/com/nzesportes/api/nzapi/repositories/product/ProductDetailRepository.java
+++ src/main/java/br/com/nzesportes/api/nzapi/repositories/product/ProductDetailRepository.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 public interface ProductDetailRepository extends JpaRepository<ProductDetails, UUID> {
 
-    @Query("SELECT pd FROM ProductDetails pd, Product p " +
+    @Query("SELECT DISTINCT (pd) FROM ProductDetails pd, Product p " +
             "INNER JOIN pd.subCategories subCategory " +
             "INNER JOIN subCategory.categories category " +
             "INNER JOIN pd.stock stock " +
@@ -20,12 +20,12 @@ public interface ProductDetailRepository extends JpaRepository<ProductDetails, U
             "AND (:category IS NULL OR category.name = :category) " +
             "AND (:productSize IS NULL OR stock.size = :productSize) " +
             "AND pd.productId = p.id " +
-            "AND (COALESCE(:nameSearch) IS NULL OR (p IN :nameSearch)) " +
+            "AND (p IN :nameSearch) " +
             "AND (:brand IS NULL OR p.brand.name = :brand) " +
             "AND (:color IS NULL OR pd.color = :color)")
     Page<ProductDetails> findByFilter(List<Product> nameSearch, Gender gender, String category, String subcategory, String productSize, String brand, String color, Pageable pageable);
 
-    @Query("SELECT pd FROM ProductDetails pd, Product p " +
+    @Query("SELECT DISTINCT (pd) FROM ProductDetails pd, Product p " +
             "INNER JOIN pd.subCategories subCategory " +
             "INNER JOIN subCategory.categories category " +
             "INNER JOIN pd.stock stock " +
