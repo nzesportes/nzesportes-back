8b32a94d994180218fd3acd579199a5d4ff64a81
nfandre <andrelive.05@hotmail.com>
2021-11-11 22:50:18 -0300

    fix: enum

--- src/main/java/br/com/nzesportes/api/nzapi/dtos/enums/EmailContentEnum.java
+++ src/main/java/br/com/nzesportes/api/nzapi/dtos/enums/EmailContentEnum.java
@@ -4,5 +4,15 @@ public enum EmailContentEnum {
     COMPRA_CONFIRMADA("Olá! Recebemos seu pedido. Somente quando recebermos a confirmação, seguiremos com o envio das suas compras. O prazo de entrega passa a ser contado somente após a confirmação do pagamento."),
     COMPRA_APROVADA("O pagamento do seu pedido foi aprovado. Agradecemos sua preferência por nossa loja!"),
     COMPRA_REJEITADA("Ops! O pagamento do seu pedido foi rejeitado. Entre em contato sua credora!"),
-    COMPRA_CANCELADA("O pagamento do seu pedido foi cacelado. Entre em contato sua credora!"),
+    COMPRA_CANCELADA("O pagamento do seu pedido foi cacelado. Entre em contato sua credora!");
+
+    private final String text;
+
+    EmailContentEnum(final String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
 }
