1ff261693f55a03e1375d0cad8907fb5a630a96f
Felipe Loureiro <felipe-loureiro@outlook.com>
2021-11-16 23:50:00 -0300

    ajustes

--- src/main/java/br/com/nzesportes/api/nzapi/services/product/CouponService.java
+++ src/main/java/br/com/nzesportes/api/nzapi/services/product/CouponService.java
@@ -1,6 +1,8 @@
 package br.com.nzesportes.api.nzapi.services.product;
 
 import br.com.nzesportes.api.nzapi.domains.product.Coupon;
+import br.com.nzesportes.api.nzapi.dtos.product.CouponDTO;
+import br.com.nzesportes.api.nzapi.errors.ResourceConflictException;
 import br.com.nzesportes.api.nzapi.errors.ResourceNotFoundException;
 import br.com.nzesportes.api.nzapi.errors.ResponseErrorEnum;
 import br.com.nzesportes.api.nzapi.repositories.product.CouponRepository;
@@ -12,17 +14,24 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import static org.springframework.beans.BeanUtils.copyProperties;
+
 @Service
 public class CouponService {
     @Autowired
     private CouponRepository repository;
 
     public Coupon save(Coupon coupon) {
+        repository.findByCode(coupon.getCode()).ifPresent(c -> {
+            throw new ResourceConflictException(ResponseErrorEnum.CONFLICTED_OPERATION);
+        });
         coupon.setQuantityLeft(coupon.getQuantity());
         return repository.save(coupon);
     }
     public Coupon update(Coupon coupon) {
-        return repository.save(coupon);
+        Coupon toSave = getById(coupon.getId());
+        copyProperties(coupon, toSave);
+        return repository.save(toSave);
     }
 
     public Page<Coupon> getCoupons(int page, int size, String code) {
@@ -33,11 +42,13 @@ public class CouponService {
         return repository.findAllById(id);
     }
 
-    public Boolean getStatus(String code) {
+    public CouponDTO getStatus(String code) {
         Coupon coupon = repository.findByCode(code).orElseThrow(() -> new ResourceNotFoundException(ResponseErrorEnum.NOT_FOUND));
-        if(coupon.getQuantityLeft() > 0 && !LocalDate.now().isBefore(coupon.getEndDate()))
-            return true;
-        return false;
+        if(coupon.getQuantityLeft() > 0
+                && !LocalDate.now().isBefore(coupon.getStartDate())
+                && !LocalDate.now().isAfter(coupon.getEndDate()))
+            return CouponDTO.builder().status(true).coupon(coupon).build();
+        return CouponDTO.builder().status(false).coupon(coupon).build();
     }
 
     public Coupon getByCode(String code) {
